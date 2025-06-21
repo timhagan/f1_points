8ed1d918449dc3ac7b848771e3a30979514f97fe
Tim Hagan <timothy.g.hagan@gmail.com>
2025-06-21 14:53:00 -0500

    fix bug in get_past_event_points
    past_event_names was reading in past event names as an array. corrected to be read in as a list

--- src/data_prep/get_past_event_points.py
+++ src/data_prep/get_past_event_points.py
@@ -7,7 +7,7 @@ from src.data_prep import functions
 def main():
     # Get all past events and process them
     try:
-        past_event_names = functions.get_past_race_event_names() or []
+        past_event_names = functions.get_past_race_event_names().tolist() or []
     except Exception:
         past_event_names = []
     for past_event_name in past_event_names:
